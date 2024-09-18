80ed0bc040c6688b40d327735cffdec35b575c31
OmarMuhammed <amorey2006@gmail.com>
2024-09-18 17:43:09 +0300

    post_system-end

---
+++ social/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Post
+#192734
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['body']
+        labels = {
+            'body': 'What’s on your mind?',  
+        }
+        widgets = {
+            'body': forms.Textarea(attrs={
+                'placeholder': 'What are you thinking about?',  
+                'style': 'background-color: #192734; color: white; border: 1px solid #555; padding: 10px; width: 100%; box-sizing: border-box;',  # Dark background and styles
+                'rows': 5, 
+            }),
+        }
--- social/views.py
+++ social/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 from accounts.models import CustomUser as User
 from django.views import View
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from django.shortcuts import redirect
+from django.contrib import messages
 
 @login_required
 def home(request):
@@ -13,14 +15,34 @@ def home(request):
 
 class PostListView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        userinfo = User.objects.get(username= request.user)
+
+        userinfo = User.objects.get(username=request.user)
         posts = Post.objects.all().order_by('-created_at')
-        print(userinfo)
-        print(posts)
+
+        form = PostForm()
+        if form.is_valid():
+            form.save()
+
         return render(request, 'home.html',{
             "posts":posts,
             "userinfo" : userinfo,
+            "form": form , 
         })
+    
+    def post(self, request, *args, **kwargs):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+
+            # link posts to user loggend In 
+            new_post.user = request.user 
+
+            new_post.save()
+            messages.success(request,'Your Added Post Successfully..')
+            return redirect('posts')
+            
+        return render(request, 'home.html',{"form":form})
+
 
 def profile(request):
     
